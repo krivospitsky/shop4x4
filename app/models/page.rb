8209cbc15b0f9346db8b9daa9a89f653506f9179
alex krivospitsky <alex@krivospitsky.ru>
2014-12-25 18:49:24 +0300

    small fixes

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,8 @@
 class Page < ActiveRecord::Base
   POSITIONS=[ "menu", "footer", "menu_and_footer"]
 	scope :enabled, -> { where(enabled: 't') }
-  scope :in_menu, -> { enabled.where('(position="menu" or position="menu_and_footer")') }
-  scope :in_footer, -> { enabled.where('(position="footer" or position="menu_and_footer")') }
+  scope :in_menu, -> { enabled.where('(position=? or position=?)', 'menu', 'menu_and_footer') }
+  scope :in_footer, -> { enabled.where('(position=? or position=?)', 'footer', 'menu_and_footer') }
   mount_uploader :image, ImageUploader
 
   include  Seoable
